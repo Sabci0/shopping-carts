bda40d39747d1074b354801a5b65c95e7cebd6db
Sabci0 <barteksabcio@gmail.com>
2022-12-15 22:50:22 +0100

    incremment and decrement scripts width 1 problem

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,31 @@
 let shopRef = document.getElementById('shop');
 
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
 let shopItemsData = [{
 
-    id:"1",
+    id:"obj1",
     name:"Casual Shirt",
     price: 45,
     desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
     img: "images/img-1.jpg"
 },
     {
-    id:"2",
+    id:"obj2",
     name:"Office Shirt",
     price: 100,
     desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
     img: "images/img-2.jpg"
 },
     {
-        id:"3",
+        id:"obj3",
         name:"T Shirt",
         price: 25,
         desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
         img: "images/img-3.jpg"
     },
     {
-        id:"4",
+        id:"obj4",
         name:"Mens Suit",
         price: 300,
         desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
@@ -31,9 +33,12 @@ let shopItemsData = [{
     },
 ]
 
+
+
 let generateShop = () =>{
     return (shopRef.innerHTML = shopItemsData.map((x)=>{
         let { id, name, price, desc, img } =x;
+        let search = basket.find(()=>x.id === id) || [];
         return `
         <div id=product-id-${id} class="shop__item">
             <img width="220" src="${img}" alt="">
@@ -43,9 +48,9 @@ let generateShop = () =>{
                 <div class="price-quantity">
                     <h2>${price}</h2>
                     <div class="buttons">
-                        <i class="bi bi-plus-lg"></i>
-                        <div id=${id} class="quantity">0</div>
-                        <i class="bi bi-dash-lg"></i>
+                        <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+                        <div id=${id} class="quantity">${search.item === undefined ? 0: search.item}</div>
+                        <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
                     </div>
                 </div>
             </div>
@@ -54,3 +59,53 @@ let generateShop = () =>{
     }).join(""));
 }
 generateShop()
+
+let increment = (id) =>{
+    let selectedItem = id;
+
+    let search = basket.find((x)=> x.id === selectedItem.id)
+
+    if(search === undefined){
+        basket.push({
+            id: selectedItem.id,
+            item:1
+        })
+    }else {
+        search.item +=1;
+
+    }
+
+    //console.log(basket);
+    update(selectedItem.id)
+
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (id) =>{
+    let selectedItem = id;
+
+    let search =basket.find((x)=> x.id === selectedItem.id)
+
+    if (search === undefined ) return;
+    else if(search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    update(selectedItem.id);
+    basket = basket.filter((x)=>x.item !==0);
+
+    localStorage.setItem("data", JSON.stringify(basket))
+}
+let update = (id) =>{
+    let search = basket.find((x) =>x.id === id)
+    console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+    calculation()
+};
+
+let calculation =()=> {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y)=>x+y, 0);
+
+};
+
+calculation();
